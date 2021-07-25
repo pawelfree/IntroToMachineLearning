2952964a8c22b76b6a687a46ce5717abe85cc3bf
Paweł Dudek <paweldudek@Pawes-MacBook-Pro.local>
2021-07-25 22:08:24 +0200

    pierwszy

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -9,25 +9,36 @@
     Sara has label 0
     Chris has label 1
 """
-    
 import sys
-from time import time
 sys.path.append("../tools/")
+
+
+from time import time
 from email_preprocess import preprocess
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 
 
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
+
+# features_train and features_test are the features for the training
+# and testing datasets,
+# labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
-
-
 
 
 #########################################################
 ### your code goes here ###
+cls = GaussianNB()
+t0 = time()
+cls.fit(features_train, labels_train)
+print("training time:", round(time()-t0, 3), "s")
 
 
+t0 = time()
+pred = cls.predict(features_test)
+print("predicting time:", round(time()-t0, 3), "s")
+
+accuracy = accuracy_score(pred, labels_test)
+
+print("accuracy : ", accuracy)
 #########################################################
-
-
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,6 +12,8 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
@@ -19,9 +21,23 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+#features_train = features_train[:len(features_train)/100] 
+#labels_train = labels_train[:len(labels_train)/100] 
 
+cls = SVC(kernel='rbf', C=10000.0)
+t0 = time()
+cls.fit(features_train, labels_train)
+print("training time:", round(time()-t0, 3), "s")
 
+t0 = time()
+pred = cls.predict(features_test)
+print("predicting time:", round(time()-t0, 3), "s")
 
+accuracy = accuracy_score(pred, labels_test)
+
+print("accuracy : ", accuracy)
+
+print(pred[10], pred[26], pred[50], sum(pred))
 #########################################################
 ### your code goes here ###
 
