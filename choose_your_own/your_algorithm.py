afbe4d8f043f1f408bcc1fda8f5c63abe64d373c
Paweł Dudek <paweldudek@Pawes-MacBook-Pro.local>
2021-07-26 19:04:23 +0200

    26.07.2021

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -3,6 +3,8 @@
 import matplotlib.pyplot as plt
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
+from time import time
+from sklearn.metrics import accuracy_score
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
@@ -31,10 +33,22 @@ plt.show()
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
 
+from sklearn.ensemble import RandomForestClassifier
 
 
+cls = RandomForestClassifier(min_samples_split=40)
+t0 = time()
+cls.fit(features_train, labels_train)
+print("training time:", round(time()-t0, 3), "s")
 
 
+t0 = time()
+pred = cls.predict(features_test)
+print("predicting time:", round(time()-t0, 3), "s")
+
+accuracy = accuracy_score(pred, labels_test)
+
+print("accuracy : ", accuracy)
 
 
 
--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -12,6 +12,8 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
@@ -20,12 +22,21 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
-
 #########################################################
 ### your code goes here ###
+cls = DecisionTreeClassifier( min_samples_split=40)
+#t0 = time()
+cls.fit(features_train, labels_train)
+#print("training time:", round(time()-t0, 3), "s")
 
 
+#t0 = time()
+pred = cls.predict(features_test)
+#print("predicting time:", round(time()-t0, 3), "s")
+
+accuracy = accuracy_score(pred, labels_test)
+
+print("accuracy : ", accuracy)
 #########################################################
 
-
+print(len(features_train[0]))
